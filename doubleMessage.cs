042afa8773499437383dfb3212bf1c6662825b1e
HOOTER <captainhooter30@gmail.com>
2023-07-16 19:55:20 -0400

    Initial commit

---
+++ doubleMessage.cs
@@ -0,0 +1,15 @@
+﻿using ConVar;
+using Harmony;
+
+public class ExamplePatch
+{
+    [HarmonyPatch(typeof(ConVar.Chat), "sayAs")]
+    private class PlayerPatch
+    {
+        [HarmonyPrefix]
+        private static void Prefix(Chat.ChatChannel targetChannel, ulong userId, string username, string message, BasePlayer player = null)
+        {
+            Chat.Broadcast(message);
+        }
+    }
+}
